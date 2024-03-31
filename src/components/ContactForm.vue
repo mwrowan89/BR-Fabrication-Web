096f317dab64eb03e22454ed375d0cd6599d1c8f
Michael <mwrowan89@gmail.com>
2024-03-30 23:17:45 -0600

    Fixed form submit buttons

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,24 +1,26 @@
 <template>
-    
-    <div class="contact">
-        <form>
-    <div class="form-element"> 
-      <label for="first">First Name</label>
-      <input id="first-name" type="text" placeholder="First Name"/>
-    </div>
-    <div class="form-element" id="last">
-      <label for="last">Last Name</label>
-      <input id="last-name" type="text" placeholder="Last Name"/>
-    </div>
-    
-    <div class="form-element">
-      <label for="question">Question</label>
-      <textarea id="question"></textarea>
-    </div>
-    <input id="submit" type="submit" value="Save" />
-    <input id="cancel" type="button" value="Cancel" />
-  </form>
-    </div>
+  <div class="contact">
+    <form>
+      <div class="form-element">
+        <label for="first">First Name</label>
+        <input id="first-name" type="text" placeholder="First Name" />
+      </div>
+      <div class="form-element" id="last">
+        <label for="last">Last Name</label>
+        <input id="last-name" type="text" placeholder="Last Name" />
+      </div>
+
+      <div class="form-element">
+        <label for="question">Question</label>
+        <textarea id="question"></textarea>
+      </div>
+      <span>
+        <input id="submit" type="submit" value="Save" />
+        <input id="cancel" type="button" value="Cancel" />
+      </span>
+
+    </form>
+  </div>
 </template>
 
 <script>
@@ -32,18 +34,13 @@
 
 <style scoped>
 .contact {
-    display: flex;
-    width: 30vw;
-    font-size: 2.5rem;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    place-items: center;
-}
-
-input {
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  width: 30vw;
+  font-size: 2.5rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  place-items: center;
 }
 
 .form-element {
@@ -67,7 +64,11 @@ input {
   height: 100px;
   width: 40rem;
 }
-
+span {
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+}
 #submit,
 #cancel {
   width: 20rem;
