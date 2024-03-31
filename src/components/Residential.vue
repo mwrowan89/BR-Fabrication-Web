e5994c0efcfe1e4b5af14700889811a269238e11
Michael <mwrowan89@gmail.com>
2024-03-31 11:36:58 -0600

    Added Hover to images

--- src/components/Residential.vue
+++ src/components/Residential.vue
@@ -52,6 +52,26 @@ img {
     padding: 20px;
 }
 
+img:hover {
+    display: table-caption;
+    transform: scale(1.1);
+    transition: transform 0.3s ease;
+}
+
+.tooltip:hover:after {
+    content: attr(title);
+    display: block;
+    position: absolute;
+    background-color: rgba(0, 0, 0, 0.8);
+    color: #fff;
+    padding: 5px;
+    border-radius: 3px;
+    z-index: 9999;
+    bottom: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+}
+
 .body {
     display: flex;
     flex-direction: column;
